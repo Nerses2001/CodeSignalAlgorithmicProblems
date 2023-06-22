85c015c02f5c3e1086071e310675230ecc16cfc7
Nerses Hepoyan <nerseshepoyan22@gmail.com>
2023-06-22 02:21:16 -0800

    6-9 Lavel

--- CodeSignal/CodeSignal.cs
+++ CodeSignal/CodeSignal.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-
+//https://app.codesignal.com/profile/nerses_h_kdd
 
 namespace CodeWrithing.CodeSignal
 {
@@ -249,6 +249,219 @@ namespace CodeWrithing.CodeSignal
 
             return firstHalfSum == secondHalfSum;
         }
+
+        //Some people are standing in a row in a park. There are trees between them which cannot be moved.
+        //Your task is to rearrange the people by their heights in a non-descending order without moving the trees.
+        //People can be very tall!
+        public int[] RearrangePeopleByHeight(int[] a)
+        {
+
+            List<int> peopleHeights = new List<int>();
+            List<int> positions = new List<int>();
+
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (a[i] != -1)
+                {
+                    peopleHeights.Add(a[i]);
+                    positions.Add(i);
+                }
+            }
+
+            CountigSort(ref peopleHeights);
+
+            int currentIndex = 0;
+
+            for (int i = 0; i < positions.Count; ++i)
+            {
+                a[positions[i]] = peopleHeights[currentIndex++];
+            }
+
+            return a;
+        }
+
+        private void CountigSort(ref List<int> inputArray)
+        {
+            if (inputArray.Count <= 1)
+            {
+                return;
+            }
+
+            int max = inputArray.Max();
+            int min = inputArray.Min();
+
+            int[] counts = new int[max - min + 1];
+            int[] res = new int[inputArray.Count];
+
+            for (int i = 0; i < inputArray.Count; ++i)
+            {
+                ++counts[inputArray[i] - min];
+
+            }
+
+            int k = 0;
+            for (int i = 0; i < counts.Length; ++i)
+            {
+                while (counts[i] != 0)
+                {
+                    res[k++] = i + min;
+                    --counts[i];
+                }
+            }
+            inputArray = res.ToList();
+        }
+
+
+        //Write a function that reverses characters in (possibly nested) parentheses in the input string.
+        //Input strings will always be well-formed with matching()s.
+        public string ReverseInParentheses(string inputString)
+        {
+            Stack<int> st = new Stack<int>();
+
+            for (int i = 0; i < inputString.Length; ++i)
+            {
+                if (inputString[i] == '(')
+                {
+                    st.Push(i);
+                }
+                else if (inputString[i] == ')')
+                {
+                    char[] A = inputString.ToCharArray();
+                    ReverseString(ref A, st.Peek() + 1, i);
+                    inputString = new string(A);
+                    st.Pop();
+                }
+
+            }
+            string res = "";
+            for (int i = 0; i < inputString.Length; i++)
+            {
+                if (inputString[i] != ')' && inputString[i] != '(')
+                {
+                    res += string.Format("{0}", inputString[i]);
+
+                }
+            }
+            return res;
+        }
+
+        void ReverseString(ref char[] A, int l, int h)
+        {
+            if (l < h)
+            {
+                char ch = A[l];
+                A[l] = A[h];
+                A[h] = ch;
+                ReverseString(ref A, l + 1, h - 1);
+            }
+        }
+
+        //Several people are standing in a row and need to be divided into two teams. The first person goes into team 1,
+        //the second goes into team 2, the third goes into team 1 again, the fourth into team 2, and so on.
+        //You are given an array of positive integers - the weights of the people.Return an array of two integers, where the first element is the total weight of team 1,
+        //and the second element is the total weight of team 2 after the division is complete.
+        public int[] DivideIntoTeams(int[] a)
+        {
+            int res2 = 0;
+            int res1 = 0;
+
+            for (int i = 1; i < a.Length; i += 2)
+            {
+                res2 += a[i];
+                res1 += a[i - 1];
+            }
+            if (a.Length % 2 != 0)
+            {
+                res1 += a.Last();
+            }
+            int[] res = { res1, res2 };
+            Console.WriteLine(res1 + " " + res2);
+            return res;
+        }
+
+
+        //Given a rectangular matrix of characters, add a border of asterisks(*) to it.
+        public  string[] AddBorder(string[] picture)
+        {
+            int m = picture[0].Length;
+            string addElement = "";
+
+            for (int i = 0; i < m + 2; ++i)
+            {
+                addElement += "*";
+            }
+            string[] result = new string[picture.Length + 2];
+
+            result[0] = addElement;
+
+            for (int i = 0; i < picture.Length; ++i)
+            {
+                result[i + 1] = string.Format("*{0}*", picture[i]);
+            }
+
+            result[result.Length - 1] = addElement;
+
+
+            return result;
+        }
+
+        // Two arrays are called similar if one can be obtained from another by
+        // swapping at most one pair of elements in one of the arrays.
+        // Given two arrays a and b, check whether they are similar.
+
+        public bool AreArraysSimilar(int[] a, int[] b)
+        {
+            int count = 0;
+
+            if (a.Length != b.Length)
+            {
+                return false;
+            }
+
+            int max = a[0];
+
+            if (a.Max() != b.Max())
+            {
+                return false;
+
+            }
+            else
+            {
+                max = a.Max();
+            }
+
+
+            int[] countsA = new int[max + 1];
+            int[] countsB = new int[max + 1];
+
+            for (int i = 0; i < a.Length; ++i)
+            {
+                ++countsA[a[i]];
+                ++countsB[b[i]];
+                if (a[i] != b[i])
+                {
+                    ++count;
+                }
+            }
+            for (int i = 0; i < countsA.Length; ++i)
+            {
+                if (countsA[i] != countsB[i])
+                {
+                    return false;
+                }
+            }
+
+            if (count > 2)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+
     }
 
 }
