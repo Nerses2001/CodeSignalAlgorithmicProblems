3a0d0064712e9f062ab91761fbabbfd1077a9ddc
Nerses Hepoyan <nerseshepoyan22@gmail.com>
2023-06-22 12:38:48 -0800

    18 Lavel

--- CodeSignal/CodeSignalPr.cs
+++ CodeSignal/CodeSignalPr.cs
@@ -89,6 +89,77 @@ namespace CodeWrithing.CodeSignal
             return new string(result);
         }
 
+        //Given two cells on the standard chess board, determine whether they have the same color or not.
+        public bool AreCellsSameColor(string cell1, string cell2)
+        {
+            if ((int)cell1[0] % 2 == (int)cell1[1] % 2 && (int)cell2[0] % 2 == (int)cell2[1] % 2)
+            {
+                return true;
+            }
+            else if ((int)cell1[0] % 2 != (int)cell1[1] % 2 && (int)cell2[0] % 2 != (int)cell2[1] % 2)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+        // FindOppositeNumber
+        // Consider integer numbers from 0 to n - 1 written down along the circle in such a way
+        // that the distance between any two neighboring numbers is equal (note that 0 and n - 1 are neighboring, too).
+        // Given n and firstNumber, find the number which is written in the radially opposite position to firstNumber.
+        public int FindOppositeNumber(int n, int firstNumber)
+        {
+            return (firstNumber + n / 2) % n;
+        }
+
+        // You have deposited a specific amount of money into your bank account.
+        // Each year your balance increases at the same growth rate.
+        // With the assumption that you don't make any additional deposits,
+        // find out how long it would take for your balance to pass a specific threshold.
+        public int CalculateTimeToReachThreshold(int deposit, int rate, int threshold)
+        {
+            double dep = deposit;
+            int k = 0;
+            while (dep < threshold)
+            {
+                dep = dep + dep * rate / 100;
+                ++k;
+            }
+            return k;
+        }
+
+        // Given a sorted array of integers a, your task is to determine which element of a is closest to all other values of a.
+        // In other words, find the element x in a, which minimizes the following sum
+        // abs(a[0] - x) + abs(a[1] - x) + ... + abs(a[a.length - 1] - x)
+        //(where abs denotes the absolute value) If there are several possible answers, output the smallest one.
+
+        public int FindMinimumSum(int[] a)
+        {
+            int n = a.Length;
+            int minSum = int.MaxValue;
+            int minElement = -1;
+
+            for (int i = 0; i < n; i++)
+            {
+                int sum = 0;
+
+                for (int j = 0; j < n; j++)
+                {
+                    sum += Math.Abs(a[i] - a[j]);
+                }
+
+                if (sum < minSum)
+                {
+                    minSum = sum;
+                    minElement = a[i];
+                }
+            }
+
+            return minElement;
+        }
 
     }
 }
